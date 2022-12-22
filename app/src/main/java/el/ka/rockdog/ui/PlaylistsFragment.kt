7b2503f5120c78471013d02c872e4c6d2fd01cdf
Ekaterina-El <hackerrussia@yandex.ru>
2022-12-22 22:30:51 +0500

    Playlists screen UI

--- app/src/main/java/el/ka/rockdog/ui/PlaylistsFragment.kt
+++ app/src/main/java/el/ka/rockdog/ui/PlaylistsFragment.kt
@@ -5,19 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import el.ka.rockdog.databinding.PlayerFragmentBinding
+import el.ka.rockdog.databinding.PlaylistsFragmentBinding
 
-class PlayerFragment: Fragment() {
-  private lateinit var binding: PlayerFragmentBinding
+class PlaylistsFragment : Fragment() {
+  private lateinit var binding: PlaylistsFragmentBinding
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    binding = PlayerFragmentBinding.inflate(layoutInflater)
+    binding = PlaylistsFragmentBinding.inflate(layoutInflater)
     return binding.root
   }
-
-
 }
