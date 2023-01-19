7b793151ab2bb70292ee48c3e867fd8d3130718d
Ekaterina-El <hackerrussia@yandex.ru>
2023-01-19 20:54:39 +0500

    Added "omnipotence" button on artist page

--- app/src/main/java/el/ka/rockdog/view/ui/artist/ArtistProfileFragment.kt
+++ app/src/main/java/el/ka/rockdog/view/ui/artist/ArtistProfileFragment.kt
@@ -3,13 +3,16 @@ package el.ka.rockdog.view.ui.artist
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.canhub.cropper.CropImageOptions
+import el.ka.rockdog.R
 import el.ka.rockdog.databinding.ArtistProfileFragmentBinding
 import el.ka.rockdog.other.CropOptions
 import el.ka.rockdog.other.GridSpacingItemDecoration
@@ -18,6 +21,7 @@ import el.ka.rockdog.service.model.BandMember
 import el.ka.rockdog.view.adapter.lists.bandMembers.BandMembersAdapter
 import el.ka.rockdog.view.adapter.lists.photos.PhotosAdapter
 import el.ka.rockdog.view.ui.BaseFragment
+import el.ka.rockdog.view.ui.fullScreenViewer.FullScreenViewerFragment
 import el.ka.rockdog.viewModel.ArtistViewModel
 
 class ArtistProfileFragment : BaseFragment() {
@@ -91,6 +95,10 @@ class ArtistProfileFragment : BaseFragment() {
     viewModel.bandMembers.removeObserver { bandMemberObserver }
   }
 
+  private fun addBandMember() {
+
+  }
+
   // region Images
   private lateinit var imageChanger: ImageChanger
 
@@ -98,10 +106,9 @@ class ArtistProfileFragment : BaseFragment() {
     viewModel.updateCover(uri)
   }
 
-  fun addPhoto() = imageChanger.change(CropOptions.freeCropImageOptions) { uri ->
+  private fun addPhoto() = imageChanger.change(CropOptions.freeCropImageOptions) { uri ->
     viewModel.addPhoto(uri)
   }
-  // endregion
 
   private fun openGallerySince(url: String) {
     val artistId = viewModel.artist.value!!.id
@@ -113,8 +120,42 @@ class ArtistProfileFragment : BaseFragment() {
     )
     findNavController(). navigate(direction)
   }
+  // endregion
+
+  // region Popup menu
+  private var popupMenu: PopupMenu? = null
+
+  private fun initPopupMenu() {
+    popupMenu = PopupMenu(context, binding.verticalMenu)
+    popupMenu?.apply {
+      this.menu.add(0, ADD_BAND_MEMBER, Menu.NONE, requireContext().getString(R.string.add_band_member))
+      this.menu.add(0, ADD_PHOTO, Menu.NONE, requireContext().getString(R.string.add_photo))
+      this.menu.add(0, ADD_SOCIAL_MEDIA, Menu.NONE, requireContext().getString(R.string.add_social_media))
+      this.menu.add(0, ADD_ALBUM, Menu.NONE, requireContext().getString(R.string.add_album))
+
+      this.setOnMenuItemClickListener {
+        when (it.itemId) {
+          ADD_BAND_MEMBER -> addBandMember()
+          ADD_PHOTO -> addPhoto()
+          else -> {}
+        }
+        return@setOnMenuItemClickListener true
+      }
+    }
+
+
+  }
 
   fun openPopupMenu() {
+    if (popupMenu == null) initPopupMenu()
+    popupMenu!!.show()
+  }
+  // endregion
 
+  companion object {
+    const val ADD_BAND_MEMBER = 1
+    const val ADD_PHOTO = 2
+    const val ADD_SOCIAL_MEDIA = 3
+    const val ADD_ALBUM = 4
   }
 }
